8cf5bb1830e8021251d9a633f8018ad73a37cf65
kronss <oleg.chaychenko@gmail.com>
2018-07-22 17:06:26 +0300

    bla bla

--- include/malloc.h
+++ include/malloc.h
@@ -4,14 +4,14 @@
 #include <sys/user.h> /* PAGE_SIZE */
 
 
-#define ALIGN_META_INFO(x) (((((x) -1) >> 2) << 2) + 4)
+#define ALIGN_META_INFO(x) (((((x) - 1) >> 2) << 2) + 4)
 
 #define BLOCK_SIZE sizeof(struct s_block)
 
 
 
 /* block struct */
-struct s_block {
+struct block_s {
 	size_t           size;
 	s_block         *next;
 	s_block         *prev;
@@ -21,24 +21,40 @@ struct s_block {
 };
 
 
-enum {
+
+
+struct malloc_meneger_s {
+    struct block_s *zone_heads[MAX_ZONE_TYPE];
+//    size_t malloc_call[MAX_ZONE_TYPE];
+};
+
+
+
+
+
+
+
+enum zone_type_e {
 	TINY = 1,
 	SMALL = 2,
-	LARGE = 3
+	LARGE = 3,
+
+	MAX_ZONE_TYPE
 };
 
 
-enum {
+enum zone_size_e {
 	TINY_ZONE  = PAGE_SIZE < 4096 ? 4096 : PAGE_SIZE,
 	SMALL_ZONE = TINY_ZONE * 100,
-	LARGE_ZONE = SMALL_ZONE + 1   //TODO: rework!
+	LARGE_ZONE = SMALL_ZONE + 1,   //TODO: rework!
 };
 
-enum {
-
+enum zone_treshold_e{
+    TINY_TRESHHOLD = TINY_ZONE / 100,
+    SMALL_TRESHHOLD = SMALL_ZONE / 100,
+    LARGE_TRESHHOLD = LARGE_ZONE / 100,
 
 };
-
 
 
 
--- src/malloc.c
+++ src/malloc.c
@@ -1,16 +1,49 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <malloc.h>
 
 pthread_mutex_t mutex_malloc = PTHREAD_MUTEX_INITIALIZER;
+struct malloc_meneger_s malloc_meneger_g =
+{
+    .zone_heads = {NULL, NULL, NULL}
+};
 
-//
-//int main()
-//{
-//	printf("%d", PAGE_SIZE);
-//}
+void *get_ptr_from_zone(size_t size, zone_type_e zone)
+{
+    void *retval;
 
+    if (!malloc_meneger_g.zone_heads[zone]) {
+        malloc_meneger_g.zone_heads[zone] = new_zone();
+        if (!malloc_meneger_g.zone_heads[zone]) {
+            return NULL;
+        }
+    }
+    retval = get_best_chunk();
+
+    return retval;
+}
+
+
+
+
+void *get_ptr(size)
+{
+    void *retval;
+
+    if (size < TINY_TRESHHOLD) {
+        retval = get_ptr_from_zone(size, TINY);
+    } else if (size < SMALL_TRESHHOLD) {
+        retval = get_ptr_from_zone(size, SMALL);
+    } else if (size < LARGE_TRESHHOLD) {
+        retval = get_ptr_from_zone(size, LARGE);
+    } else {
+        ;
+    }
+
+
+    return retval;
+}
 
 
 
@@ -19,32 +52,24 @@ void *malloc(size_t size)
 	void *retval;
 
 	pthread_mutex_lock(&mutex_malloc);
-
+	retval = NULL;
 	if (size <= 0) {
-		pthread_mutex_unlock(&mutex_malloc);
-		return (void *)0x0L;
+	    goto end;
 	}
 
 	if (size < (size_t)-32) {
 		ALIGN_META_INFO(size);
 	}
 
-
-	if () {
-
-	} else if () {
-
-	} else {
-
-	}
+    get_ptr(size);
 
 
-
-	pthread_mutex_unlock(&mutex_malloc);
 	if (NULL == retval) {
 //		errno = ENOMEM;
 
 	}
-)
-	return (ptr);
+
+end:
+    pthread_mutex_unlock(&mutex_malloc);
+    return retval;
 }
